2b058bbdf736b1ae7668aff4999a35234448f435
Dirk Lannatewitz <dirk@lannatewitz.biz>
2023-05-09 15:44:23 +0200

    change main.tf in module

--- modules/acr/main.tf
+++ modules/acr/main.tf
@@ -1,7 +1,7 @@
-resource "null_resource" "docker_pull" {
-   provisioner "local-exec" {
-     command = <<-EOT    
-       docker pull ghcr.io/worldhealthorganization/ddcc-gateway/ddcc-gateway:1.5.0-9a461d9
-     EOT
-   }
+resource "null_resource" "docker_push" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker pull ghcr.io/worldhealthorganization/ddcc-gateway/ddcc-gateway:1.5.0-9a461d9
+    EOT
+  }
 }
